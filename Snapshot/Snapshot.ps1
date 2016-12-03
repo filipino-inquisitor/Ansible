96c3ca8feee165f487132fa28120d1990db2d037
Christian Narvios <christian.narvios@gmail.com>
2016-12-02 21:02:35 -0500

    Update Snapshot.ps1

--- Snapshot/Snapshot.ps1
+++ Snapshot/Snapshot.ps1
@@ -1,1 +1,4 @@
-New-EC2Snapshot-VolumeId vol-0fd39a57fccae6e2a -Description "Snapshot created Date"
+Param(
+[string]$access, [string]$secret)
+
+New-EC2Snapshot -VolumeID vol-0fd39a57fccae6e2a -Description "Snapshot created Date" -Region us-east-1 -SecretKey $secret -AccessKey $access
